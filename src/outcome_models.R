f615f2837c7674c343164ea050b8d85f13a05835
Matthew Pryce <lsh1800165@lshtm.ac.uk>
2024-03-22 12:20:46 +0000

    Updating start info

--- src/outcome_models.R
+++ src/outcome_models.R
@@ -21,25 +21,19 @@ library(SuperLearner)
 library(mice)
 
 
-#' @param analysis Type of analysis to be run (e.g. complete case or outcome imputation) 
 #' @param data The data frame containing all required information
 #' @param id Identification for individuals
 #' @param outcome The name of the outcome of interest
 #' @param exposure The name of the exposure of interest
-#' @param outcome_observed_indicator Indicator identifying when the outcome variable is observed (1=observed, 0=missing)
 #' @param out_method Statistical technique used to run the outcome models
 #' @param out_covariates  List containing the names of the variables to be input into each outcome model
 #' @param out_SL_lib Library to be used in super learner if selected
 #' @param out_SL_strat Indicator for if stratification should be used in the super learner CV (stratifies outcomes - binary only)
-#' @param oracle If an oracle version of the T-learner is to be run
-#' @param Y.0 If oracle=1, Y.0 value to be used in oracle model
-#' @param Y.1 If oracle=1, Y.1 value to be used in oracle model
-#' @param imp_covariates Covariates to be used in SL imputation model if SL imputation used
-#' @param imp_SL_lib SL libaray for imputation model if SL imputation used
-#' @param imp_SL_strat Whether SL CV folds are stratified in the imputation model if SL imputation used  
-#' @param newdata New data to create predictions for
+#' @param Y_bin Indicator for hether outcome is binary
+#' @param Y_cont Indicator for hether outcome is continuous 
+#' @param pred_data New data to create predictions for
 
-#' @return A dataset containing n CATE estimates (UPDATE) 
+#' @return Outcome models and their predictions for pred_data 
 
 
 out_mods <- function(data,
@@ -52,9 +46,6 @@ out_mods <- function(data,
                      out_SL_strat = FALSE,
                      Y_bin,
                      Y_cont,
-                     nuisance_estimates_input = 0,
-                     o_0_pred = NA,
-                     o_1_pred = NA,
                      pred_data
 ){
   
--- src/pseudo_outcome_model.R
+++ src/pseudo_outcome_model.R
@@ -21,25 +21,16 @@ library(SuperLearner)
 library(mice)
 
 
-#' @param analysis Type of analysis to be run (e.g. complete case or outcome imputation) 
 #' @param data The data frame containing all required information
 #' @param id Identification for individuals
 #' @param outcome The name of the outcome of interest
 #' @param exposure The name of the exposure of interest
-#' @param outcome_observed_indicator Indicator identifying when the outcome variable is observed (1=observed, 0=missing)
-#' @param out_method Statistical technique used to run the outcome models
-#' @param out_covariates  List containing the names of the variables to be input into each outcome model
-#' @param out_SL_lib Library to be used in super learner if selected
-#' @param out_SL_strat Indicator for if stratification should be used in the super learner CV (stratifies outcomes - binary only)
-#' @param oracle If an oracle version of the T-learner is to be run
-#' @param Y.0 If oracle=1, Y.0 value to be used in oracle model
-#' @param Y.1 If oracle=1, Y.1 value to be used in oracle model
-#' @param imp_covariates Covariates to be used in SL imputation model if SL imputation used
-#' @param imp_SL_lib SL libaray for imputation model if SL imputation used
-#' @param imp_SL_strat Whether SL CV folds are stratified in the imputation model if SL imputation used  
-#' @param newdata New data to create predictions for
+#' @param pse_method Statistical technique used to run the pseudo outcome model
+#' @param pse_covariates List containing the names of the variables to be input into the pseudo outcome model
+#' @param pse_SL_lib Library to be used in super learner if selected for pseudo outcome model
+#' @param pred_data New data to create predictions for
 
-#' @return A dataset containing n CATE estimates (UPDATE) 
+#' @return Pseudo-outcome model and predictions from it for pred_data 
 
 
 Pseudo_mod <- function(data,
@@ -49,7 +40,6 @@ Pseudo_mod <- function(data,
                        pse_method = c("Parametric","Random forest","Super Learner"),
                        pse_covariates,
                        pse_SL_lib,
-                       nuisance_estimates_input = 0,
                        pred_data
 ){
   
